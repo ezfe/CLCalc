28c2e02f05676f4085869961f4a7073a887036d4
Ezekiel Elin <ezekielelin@me.com>
2016-02-22 16:01:55 -0500

    Initial Commit

---
+++ CLCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLCalc
+//
+//  Created by Ezekiel Elin on 2/22/16.
+//  Copyright © 2016 Ezekiel Elin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
