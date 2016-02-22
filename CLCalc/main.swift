bfaabd52e881c1848ce64b015d758a2344039377
Ezekiel Elin <ezekielelin@me.com>
2016-02-22 17:40:11 -0500

    Some logic

--- CLCalc/main.swift
+++ CLCalc/main.swift
@@ -8,5 +8,178 @@
 
 import Foundation
 
-print("Hello, World!")
+//http://stackoverflow.com/a/28341290/2059595
+func iterateEnum<T: Hashable>(_: T.Type) -> AnyGenerator<T> {
+    var i = 0
+    return AnyGenerator {
+        let next = withUnsafePointer(&i) { UnsafePointer<T>($0).memory }
+        i += 1
+        return next.hashValue == i - 1 ? next : nil
+    }
+}
 
+//http://stackoverflow.com/a/30231578/2059595
+extension String {
+    func replace(string:String, replacement:String) -> String {
+        return self.stringByReplacingOccurrencesOfString(string, withString: replacement, options: NSStringCompareOptions.LiteralSearch, range: nil)
+    }
+    
+    func removeWhitespace() -> String {
+        return self.replace(" ", replacement: "")
+    }
+}
+
+enum Operator: String {
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    
+    func eval(l: Double, _ r: Double) -> Double {
+        switch self {
+        case .Add:
+            return l + r
+        case .Subtract:
+            return l - r
+        case .Divide:
+            return l / r
+        case .Multiply:
+            return l * r
+        }
+    }
+}
+
+enum ProblemItems: Equatable {
+    case Number(Double)
+    case Op(Operator)
+}
+
+func ==(lhs: ProblemItems, rhs: ProblemItems) -> Bool {
+    switch(lhs, rhs) {
+    case (.Number(_), .Number(_)):
+        return true
+    case (.Op(_), .Op(_)):
+        return true
+    default:
+        return false
+    }
+}
+
+func ===(lhs: ProblemItems, rhs: ProblemItems) -> Bool {
+    switch(lhs, rhs) {
+    case (.Number(let n1), .Number(let n2)) where n1 == n2:
+        return true
+    case (.Op(let o1), .Op(let o2)) where o1 == o2:
+        return true
+    default:
+        return false
+    }
+}
+
+
+class UserInputObject {
+    let originalInput: String
+    var rawItems = [ProblemItems]()
+    
+    init?(_ input: String) {
+        self.originalInput = input.removeWhitespace()
+        
+        var operations = [Operator]()
+        
+        let inptArray = originalInput.characters.split { char in
+            for e in iterateEnum(Operator) {
+                if e.rawValue == String(char) {
+                    operations.append(e)
+                    return true
+                }
+            }
+            return false
+            }.map(String.init)
+        
+        for (i, n) in inptArray.enumerate() {
+            guard let number = Double(n) else {
+                return nil
+            }
+            
+            self.rawItems.append(ProblemItems.Number(number))
+            
+            if i < operations.count {
+                self.rawItems.append(ProblemItems.Op(operations[i]))
+            }
+        }
+        
+        guard let l = self.rawItems.last where l == .Number(0) else {
+            return nil
+        }
+    }
+    
+    func eval() -> Double {
+        return eval(self.rawItems)
+    }
+    
+    func eval(input: [ProblemItems]) -> Double {
+        if input.count == 1 {
+            switch input[0] {
+            case .Number(let n):
+                return n
+            case .Op(_):
+                print("A critical error occured, the input was an operator")
+                return 0
+            }
+        }
+        
+        var firstPrimary: Int? = nil
+        var firstSecondary: Int? = nil
+        
+        for (i,item) in input.enumerate() {
+            switch item {
+            case .Number(_):
+                continue
+            case .Op(let oper):
+                if firstPrimary == nil && (oper == .Multiply || oper == .Divide) {
+                    firstPrimary = i
+                } else if firstSecondary == nil && (oper == . Add || oper == .Subtract) {
+                    firstSecondary = i
+                }
+            }
+        }
+        
+        guard let evalAround = firstSecondary ?? firstPrimary else {
+            print("A critical error occurred finding the evalulation point (no operators most likely issue)")
+            return 0
+        }
+        
+        let finalOper: Operator
+        
+        switch input[evalAround] {
+        case .Number(_):
+            print("A critical error occured, the evaluation point is a number")
+            return 0
+        case .Op(let o):
+            finalOper = o
+        }
+        
+        let left = self.eval(Array(input.prefixUpTo(evalAround)))
+        let right = self.eval(Array(input.suffixFrom(evalAround + 1)))
+
+        let res = finalOper.eval(left, right)
+        
+        return res
+    }
+}
+
+while true {
+    print("> ", terminator: "")
+    guard let input = readLine(stripNewline: true) where input != "" else {
+        print("! No input was provided")
+        continue
+    }
+    
+    guard let uip = UserInputObject(input) else {
+        print("! Unable to parse input")
+        continue
+    }
+    
+    print(uip.rawItems)
+    print(uip.eval())
+}
